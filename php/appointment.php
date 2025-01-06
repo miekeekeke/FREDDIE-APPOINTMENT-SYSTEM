b97bffc853c282a66e5bd327ece153acffcdda65
agtundd <ag.tundd@gmail.com>
2025-01-06 14:23:32 +0800

    Create appointment.php

---
+++ php/appointment.php
@@ -0,0 +1,17 @@
+<?php
+    include 'conn.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Appointment</title>
+</head>
+<body>
+    shabu
+<button onclick="location.href='index.php'">Home</button>
+
+</body>
+</html>
